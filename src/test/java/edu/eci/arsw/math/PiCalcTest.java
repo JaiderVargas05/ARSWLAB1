6572d6cc55eab42a27deff247c07ab7afac72d84
MateoSebF <mateo.forero-f@mail.escuelaing.edu.co>
2025-01-26 13:30:25 -0500

    Segunda implemtnacion

--- src/test/java/edu/eci/arsw/math/PiCalcTest.java
+++ src/test/java/edu/eci/arsw/math/PiCalcTest.java
@@ -40,7 +40,7 @@ public class PiCalcTest {
         for (int start = 0; start < expected.length; start++) {
             for (int count = 0; count < expected.length - start; count++) {
                 //System.out.println("Testing " + start + " " + count);
-                byte[] digits = PiDigits.getDigits(start, count, 2);
+                byte[] digits = PiDigits.getDigits(start, count, 1);
                 assertEquals(count, digits.length);
 
                 for (int i = 0; i < digits.length; i++) {
